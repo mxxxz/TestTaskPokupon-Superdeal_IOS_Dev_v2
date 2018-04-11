f9a4981ecd0ec0127ac3ae61557eefeeffd84a9d
Dmitry Grusha <dmitrygrusha@macbook-pro-dmitry.local>
2018-04-11 17:29:16 +0300

    Initial Commit

---
+++ TestTaskPokupon&Superdeal_IOS_Dev_v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTaskPokupon&Superdeal_IOS_Dev_v2
+//
+//  Created by Dmitry Grusha on 11.04.2018.
+//  Copyright © 2018 Dmitry Grusha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
